4af8d279d8708b57ea9ae619f3a4a02715f5ad34
Heather Piwowar <hpiwowar@gmail.com>
2018-07-10 17:36:30 -0700

    add scaling

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -3,8 +3,12 @@
 echo "vac pub_queue";
 . $HOME/.bash_profile;
 . $HOME/.bashrc;
+
+sudo sh install_heroku_cli.sh;
+
+heroku ps:scale update:0 --app=oadoi
+
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
 psql $DATABASE_URL -c "vacuum verbose analyze pub_queue"
 
-. install_heroku_cli.sh;
-
+heroku ps:scale update:30 --app=oadoi
