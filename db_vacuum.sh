ac9a900ac914f946fc0154275d4fd7999da9e4e6
Heather Piwowar <hpiwowar@gmail.com>
2018-07-11 07:31:32 -0700

    add killall

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -8,6 +8,8 @@ sudo sh $HOME/unpaywall-export/install_heroku_cli.sh;
 
 heroku ps:scale update=0 --app=oadoi
 
+heroku pg:killall --app=oadoi
+
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
 psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
 
