6cc39aadb07aff5dfc9a8d0a61bddc25b94c59c5
Heather Piwowar <hpiwowar@gmail.com>
2018-07-11 07:31:05 -0700

    add dir

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -4,11 +4,11 @@ echo "vac pub_queue";
 . $HOME/.bash_profile;
 . $HOME/.bashrc;
 
-sudo sh install_heroku_cli.sh;
+sudo sh $HOME/unpaywall-export/install_heroku_cli.sh;
 
 heroku ps:scale update=0 --app=oadoi
 
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
-psql $DATABASE_URL -c "vacuum verbose analyze pub_queue"
+psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
 
 heroku ps:scale update=30 --app=oadoi
