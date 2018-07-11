4f0c782ff42b000b4c6e37d69075cd8a19d4f7de
Heather Piwowar <hpiwowar@gmail.com>
2018-07-10 17:42:15 -0700

    typo

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -6,9 +6,9 @@ echo "vac pub_queue";
 
 sudo sh install_heroku_cli.sh;
 
-heroku ps:scale update:0 --app=oadoi
+heroku ps:scale update=0 --app=oadoi
 
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
 psql $DATABASE_URL -c "vacuum verbose analyze pub_queue"
 
-heroku ps:scale update:30 --app=oadoi
+heroku ps:scale update=30 --app=oadoi
