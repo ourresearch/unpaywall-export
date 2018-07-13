849b64795f6a7fb810e64368c62d09bd2b08807f
Heather Piwowar <hpiwowar@gmail.com>
2018-07-13 10:15:55 -0700

    remove killall for now

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -11,7 +11,7 @@ heroku --version
 
 heroku ps:scale update=0 --app=oadoi
 
-heroku pg:killall --app=oadoi
+# heroku pg:killall --app=oadoi
 
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
 psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
