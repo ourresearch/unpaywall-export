6c89953962892d1b80a6de04ab96253d0c4e8663
Richard Orr <richard@tate.rs>
2019-03-03 20:56:18 -0800

    remember number of update workers

--- db_vacuum.sh
+++ db_vacuum.sh
@@ -9,6 +9,8 @@ alias heroku="/usr/local/bin/heroku"
 # check we have the needed heroku
 heroku --version
 
+UPDATE_WORKERS=$(heroku ps -a oadoi update | grep '^update' | wc -l)
+
 heroku ps:scale update=0 --app=oadoi
 
 # heroku pg:killall --app=oadoi
@@ -16,6 +18,6 @@ heroku ps:scale update=0 --app=oadoi
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
 psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
 
-heroku ps:scale update=15 --app=oadoi
+heroku ps:scale update=$UPDATE_WORKERS --app=oadoi
 
 psql $DATABASE_URL -c "vacuum verbose analyze pub"
