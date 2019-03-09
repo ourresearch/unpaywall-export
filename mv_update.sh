c9c750103f4a7c33cb11420ab7e82c7cc73aab18
Richard Orr <richard@tate.rs>
2019-03-09 09:38:45 -0800

    monitor overdue refresh fraction

--- mv_update.sh
+++ mv_update.sh
@@ -8,4 +8,5 @@ echo "refresh materialized views for metrics";
 psql $DATABASE_URL -c "refresh materialized view pub_refresh_priority_histo_mv";
 psql $DATABASE_URL -c "refresh materialized view pub_refresh_rate_mv";
 psql $DATABASE_URL -c "refresh materialized view pub_update_rate_mv";
+psql $DATABASE_URL -c "insert into pub_refresh_overdue_fraction (select now(),  1.0 * (select sum(count) from pub_refresh_priority_histo_mv where priority > 1) / (select sum(count) from pub_refresh_priority_histo_mv))";
 
