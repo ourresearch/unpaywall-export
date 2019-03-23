82c54cb673f8141564e20342d9cc2a6f6ba079dc
Richard Orr <richard@tate.rs>
2019-03-23 10:57:29 -0700

    reorder queue maintenance operations

--- pub_queue_maintenance.sh
+++ pub_queue_maintenance.sh
@@ -16,13 +16,15 @@ heroku ps:scale update=0 --app=oadoi
 heroku ps:scale refresh=0 --app=articlepage
 
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
-psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
-psql $DATABASE_URL -c "vacuum verbose analyze pub"
-psql $DATABASE_URL -c "update pub_queue set started = null where started is not null"
 
-psql $DATABASE_URL -c "vacuum full verbose analyze pub_refresh_queue"
 psql $DATABASE_URL -c "update pub_refresh_queue set started = null where started is not null"
+psql $DATABASE_URL -c "vacuum full verbose analyze pub_refresh_queue"
 
-heroku ps:scale update=$UPDATE_WORKERS --app=oadoi
 heroku ps:scale refresh=$REFRESH_WORKERS --app=articlepage
 
+psql $DATABASE_URL -c "update pub_queue set started = null where started is not null"
+psql $DATABASE_URL -c "vacuum full verbose analyze pub_queue"
+
+heroku ps:scale update=$UPDATE_WORKERS --app=oadoi
+
+psql $DATABASE_URL -c "vacuum verbose analyze pub"
