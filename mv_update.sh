aaa8dd079229f65eaaea4061ef2c129cf5a4d3af
Richard Orr <richard@tate.rs>
2019-03-25 10:38:42 -0700

    refresh pdf and green scrape metrics

--- mv_update.sh
+++ mv_update.sh
@@ -8,5 +8,7 @@ echo "refresh materialized views for metrics";
 psql $DATABASE_URL -c "refresh materialized view pub_refresh_priority_histo_mv";
 psql $DATABASE_URL -c "refresh materialized view pub_refresh_rate_mv";
 psql $DATABASE_URL -c "refresh materialized view pub_update_rate_mv";
+psql $DATABASE_URL -c "refresh materialized view green_scrape_rate_mv";
+psql $DATABASE_URL -c "refresh materialized view pdf_check_rate_mv";
 psql $DATABASE_URL -c "insert into pub_refresh_overdue_fraction (select now(),  1.0 * (select sum(count) from pub_refresh_priority_histo_mv where priority > 1) / (select sum(count) from pub_refresh_priority_histo_mv))";
 
