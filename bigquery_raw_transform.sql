0297a1973e0542a0ec37e191f510224ed028caad
Richard Orr <richard@tate.rs>
2019-03-28 15:53:37 -0700

    add bigquery changefile load script

---
+++ bigquery_raw_transform.sql
@@ -0,0 +1,94 @@
+#standardSQL
+create temp function extract_oa_location(json string) AS (
+  (
+    select
+      case when json is null then null
+      else struct(
+        cast(json_extract_scalar(json, '$.updated') as datetime) as updated,
+        json_extract_scalar(json, '$.url') as url,
+        json_extract_scalar(json, '$.url_for_pdf') as url_for_pdf,
+        json_extract_scalar(json, '$.url_for_landing_page') as url_for_landing_page,
+        json_extract_scalar(json, '$.evidence') as evidence,
+        json_extract_scalar(json, '$.license') as license,
+        json_extract_scalar(json, '$.version') as version,
+        json_extract_scalar(json, '$.host_type') as host_type,
+        cast(json_extract_scalar(json, '$.is_best') as bool) as is_best,
+        json_extract_scalar(json, '$.pmh_id') as pmh_id,
+        json_extract_scalar(json, '$.endpoint_id') as endpoint_id
+      )
+      end as oa_location
+  )
+);
+
+create temp function extract_oa_locations(json string) AS (
+  (
+    select array_agg(locations ignore nulls) as oa_locations
+    from unnest ([
+      extract_oa_location(json_extract(json, '$[0]')),
+      extract_oa_location(json_extract(json, '$[1]')),
+      extract_oa_location(json_extract(json, '$[2]')),
+      extract_oa_location(json_extract(json, '$[3]')),
+      extract_oa_location(json_extract(json, '$[4]')),
+      extract_oa_location(json_extract(json, '$[5]')),
+      extract_oa_location(json_extract(json, '$[6]')),
+      extract_oa_location(json_extract(json, '$[7]')),
+      extract_oa_location(json_extract(json, '$[8]')),
+      extract_oa_location(json_extract(json, '$[9]'))
+    ]) locations
+  )
+);
+
+create temp function extract_author(json string) AS (
+  (
+    select
+      case when json is null then null
+      else struct(
+        json_extract_scalar(json, '$.given') as given,
+        json_extract_scalar(json, '$.family') as family,
+        json_extract_scalar(json, '$.ORCID') as ORCID,
+        cast(json_extract_scalar(json, '$.authenticated-orcid') as bool) as authenticated_orcid
+      )
+      end as oa_location
+  )
+);
+
+create temp function extract_authors(json string) AS (
+  (
+    select array_agg(authors ignore nulls) as authors
+    from unnest ([
+      extract_author(json_extract(json, '$[0]')),
+      extract_author(json_extract(json, '$[1]')),
+      extract_author(json_extract(json, '$[2]')),
+      extract_author(json_extract(json, '$[3]')),
+      extract_author(json_extract(json, '$[4]')),
+      extract_author(json_extract(json, '$[5]')),
+      extract_author(json_extract(json, '$[6]')),
+      extract_author(json_extract(json, '$[7]')),
+      extract_author(json_extract(json, '$[8]')),
+      extract_author(json_extract(json, '$[9]'))
+    ]) authors
+  )
+);
+
+select
+  json_extract_scalar(data, '$.doi') as doi,
+  concat('https://doi.org/', json_extract_scalar(data, '$.doi')) as doi_url,
+  cast(json_extract_scalar(data, '$.is_oa') as bool) as is_oa,
+  json_extract_scalar(data, '$.oa_status') as oa_status,
+  extract_oa_location(json_extract(data, '$.best_oa_location')) as best_oa_location,
+  extract_oa_locations(json_extract(data, "$.oa_locations")) as oa_locations,
+  cast(json_extract_scalar(data, '$.data_standard') as INT64) as data_standard,
+  json_extract_scalar(data, '$.title') as title,
+  json_extract_scalar(data, '$.year') as year,
+  cast(json_extract_scalar(data, '$.journal_is_oa') as bool) as journal_is_oa,
+  cast(json_extract_scalar(data, '$.journal_is_in_doaj') as bool) as journal_is_in_doaj,
+  json_extract_scalar(data, '$.journal_issns') as journal_issns,
+  json_extract_scalar(data, '$.journal_name') as journal_name,
+  json_extract_scalar(data, '$.publisher') as publisher,
+  cast(replace(json_extract(data, '$.published_date'), '"', '') as date) as published_date,
+  cast(replace(json_extract(data, '$.updated'), '"', '') as datetime) as updated,
+  json_extract_scalar(data, '$.genre') as genre,
+  extract_authors(json_extract(data, '$.z_authors')) as z_authors,
+  json_extract(data, '$') as json_data
+from `__API_RAW_STAGING_TABLE__`
+;
---
+++ update_bigquery_api_live.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+usage() {
+    echo "
+Usage: $0
+load the the last 2 days of api response changes to bigquery
+
+The following environmental variables are required:
+
+DATABASE_URL     connection string for database
+"
+}
+
+logger() {
+    echo "$(date --utc +'%Y-%m-%dT%H:%M:%S') : $1"
+}
+
+if [[ "$DATABASE_URL" == "" ]]; then
+    echo "Missing DATABASE_URL environment variable"
+    usage
+    exit 1
+fi
+
+require_success() {
+    if [[ $1 -ne 0 ]] ; then
+        logger "Error $1 while running $3"
+        exit $2
+    fi
+}
+
+FILENAME=$(mktemp -t bigquery_changefile_XXXXXX.jsonl)
+MIN_CHANGED=$(date --utc --date '2 days ago' +'%Y-%m-%dT%H:%M:%S')
+
+logger "Export changes since $MIN_CHANGED to $FILENAME"
+
+/usr/bin/psql "${DATABASE_URL}" -c "\copy (select response_jsonb from pub where response_jsonb is not null and last_changed_date between '$MIN_CHANGED'::timestamp and now() and updated > '1043-01-01'::timestamp) to '${FILENAME}';"
+require_success $? 2 'psql'
+
+logger "Created $FILENAME: $(stat -c%s """$FILENAME""") bytes"
+logger "wc on $FILENAME: $(wc -l < """$FILENAME""") lines"
+
+logger "Cleaning, fixing bad characters"
+sed -i '/^\s*$/d' "$FILENAME"
+sed -i 's:\\\\:\\:g' "$FILENAME"
+
+TEMP_SUFFIX=$RANDOM
+STAGING_TABLE="unpaywall.api_changes_staging_${TEMP_SUFFIX}"
+logger "uploading changefile $FILENAME to staging table $STAGING_TABLE"
+bq load --source_format=CSV -F '\t' --schema 'data:string' $STAGING_TABLE $FILENAME
+require_success $? 3 'bq'
+
+EXTRACTED_STAGING_TABLE="unpaywall.api_changes_extracted_staging_${TEMP_SUFFIX}"
+logger "extracting $STAGING_TABLE to $EXTRACTED_STAGING_TABLE"
+bq query --max_rows=0 --destination_table="$EXTRACTED_STAGING_TABLE" "$(cat bigquery_raw_transform.sql | sed "s/__API_RAW_STAGING_TABLE__/${STAGING_TABLE}/")"
+require_success $? 4 'bq'
+
+logger "delete raw staging table $STAGING_TABLE"
+bq rm -f $STAGING_TABLE
+require_success $? 5 'bq'
+
+LIVE_TABLE="unpaywall.api_live"
+logger "delete updated rows from live table $LIVE_TABLE"
+bq query --use_legacy_sql=false "delete from $LIVE_TABLE where doi in (select doi from $EXTRACTED_STAGING_TABLE);"
+require_success $? 6 'bq'
+
+logger "insert updated rows from $EXTRACTED_STAGING_TABLE to live table $LIVE_TABLE"
+bq query --use_legacy_sql=false "\
+    insert into $LIVE_TABLE (\
+        doi, doi_url, is_oa, oa_status, best_oa_location, oa_locations, data_standard, title, year, journal_is_oa, journal_is_in_doaj, \
+        journal_issns, journal_name, publisher, published_date, updated, genre, z_authors, json_data \
+    ) select * from $EXTRACTED_STAGING_TABLE;"
+require_success $? 7 'bq'
+
+logger "delete extracted staging table $EXTRACTED_STAGING_TABLE"
+bq rm -f $EXTRACTED_STAGING_TABLE
+require_success $? 8 'bq'
