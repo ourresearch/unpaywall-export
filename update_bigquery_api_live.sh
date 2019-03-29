86d3aa10e9dcccfce2a66bf7711c4baa190fbad6
Richard Orr <richard@tate.rs>
2019-03-28 17:15:51 -0700

    quiet the bigquery update

--- update_bigquery_api_live.sh
+++ update_bigquery_api_live.sh
@@ -46,25 +46,25 @@ sed -i 's:\\\\:\\:g' "$FILENAME"
 TEMP_SUFFIX=$RANDOM
 STAGING_TABLE="unpaywall.api_changes_staging_${TEMP_SUFFIX}"
 logger "uploading changefile $FILENAME to staging table $STAGING_TABLE"
-bq load --source_format=CSV -F '\t' --schema 'data:string' $STAGING_TABLE $FILENAME
+bq --headless --quiet load --source_format=CSV -F '\t' --schema 'data:string' $STAGING_TABLE $FILENAME
 require_success $? 3 'bq'
 
 EXTRACTED_STAGING_TABLE="unpaywall.api_changes_extracted_staging_${TEMP_SUFFIX}"
 logger "extracting $STAGING_TABLE to $EXTRACTED_STAGING_TABLE"
-bq query --max_rows=0 --destination_table="$EXTRACTED_STAGING_TABLE" "$(cat bigquery_raw_transform.sql | sed "s/__API_RAW_STAGING_TABLE__/${STAGING_TABLE}/")"
+bq --headless --quiet query --max_rows=0 --destination_table="$EXTRACTED_STAGING_TABLE" "$(cat bigquery_raw_transform.sql | sed "s/__API_RAW_STAGING_TABLE__/${STAGING_TABLE}/")"
 require_success $? 4 'bq'
 
 logger "delete raw staging table $STAGING_TABLE"
-bq rm -f $STAGING_TABLE
+bq --headless --quiet rm -f $STAGING_TABLE
 require_success $? 5 'bq'
 
 LIVE_TABLE="unpaywall.api_live"
 logger "delete updated rows from live table $LIVE_TABLE"
-bq query --use_legacy_sql=false "delete from $LIVE_TABLE where doi in (select doi from $EXTRACTED_STAGING_TABLE);"
+bq --headless --quiet query --use_legacy_sql=false "delete from $LIVE_TABLE where doi in (select doi from $EXTRACTED_STAGING_TABLE);"
 require_success $? 6 'bq'
 
 logger "insert updated rows from $EXTRACTED_STAGING_TABLE to live table $LIVE_TABLE"
-bq query --use_legacy_sql=false "\
+bq --headless --quiet query --use_legacy_sql=false "\
     insert into $LIVE_TABLE (\
         doi, doi_url, is_oa, oa_status, best_oa_location, oa_locations, data_standard, title, year, journal_is_oa, journal_is_in_doaj, \
         journal_issns, journal_name, publisher, published_date, updated, genre, z_authors, json_data \
@@ -72,5 +72,5 @@ bq query --use_legacy_sql=false "\
 require_success $? 7 'bq'
 
 logger "delete extracted staging table $EXTRACTED_STAGING_TABLE"
-bq rm -f $EXTRACTED_STAGING_TABLE
+bq --headless --quiet rm -f $EXTRACTED_STAGING_TABLE
 require_success $? 8 'bq'
