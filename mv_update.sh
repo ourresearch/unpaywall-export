e59c65dde5e1a8bccbd94cb4d1235ff3bdd2d179
Richard Orr <richard@tate.rs>
2019-11-08 16:47:14 -0500

    log the number of updated pubs and how many changed

--- mv_update.sh
+++ mv_update.sh
@@ -12,3 +12,10 @@ psql $DATABASE_URL -c "refresh materialized view green_scrape_rate_mv";
 psql $DATABASE_URL -c "refresh materialized view pdf_check_rate_mv";
 psql $DATABASE_URL -c "insert into pub_refresh_overdue_fraction (select now(),  1.0 * (select sum(count) from pub_refresh_priority_histo_mv where priority > 1) / (select sum(count) from pub_refresh_priority_histo_mv))";
 
+psql $DATABASE_URL -c "\
+    insert into pub_volatility_2_day ( \
+        select \
+            now() as time, \
+            (select count(1) from pub_queue where finished > now() - '2 days'::interval and started is null) as updated, \
+            (select count(1) from pub where last_changed_date > now() - '2 days'::interval) as changed \
+    );"
