60cff22bd0134eb7eeac58f382f66955dfad5a94
Richard Orr <richard@tate.rs>
2019-12-19 11:39:17 -0500

    log doaj updates

--- load-doaj-journals.sql
+++ load-doaj-journals.sql
@@ -1,5 +1,11 @@
 \set ON_ERROR_STOP on
 
+begin;
+
+create temp table old_doaj as (
+    select issn, e_issn, year from doaj_journals
+);
+
 create temp table doaj_csv (
     title       text,
     alt_title   text,
@@ -13,3 +19,35 @@ create temp table doaj_csv (
 
 delete from doaj_journals;
 insert into doaj_journals (select * from doaj_csv);
+
+insert into logs.doaj_updates (select now(), 'XXXX-XXXX', null, null);
+
+insert into logs.doaj_updates (
+    select
+        now() as update_time,
+        issn,
+        old.year as old_year,
+        new.year as new_year
+    from
+        old_doaj old
+        full outer join doaj_journals new using (issn)
+    where
+        issn is not null
+        and old.year is distinct from new.year
+);
+
+insert into logs.doaj_updates (
+    select
+        now() as update_time,
+        e_issn,
+        old.year as old_year,
+        new.year as new_year
+    from
+        old_doaj old
+        full outer join doaj_journals new using (e_issn)
+    where
+        e_issn is not null
+        and old.year is distinct from new.year
+);
+
+commit;
