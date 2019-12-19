59efc75588ef70136b8ba9ceaca5e37bdb160e75
Richard Orr <richard@tate.rs>
2019-12-19 14:21:06 -0500

    log green scrape errors by repo

--- log-green-scrape-stats.sql
+++ log-green-scrape-stats.sql
@@ -3,14 +3,32 @@ begin;
 insert into logs.green_scrape (
     select
         now() as time ,
-        count(1) as pages_scraped_last_hour,
-        sum(case when error is not null and error != '' then 1 else 0 end) as errors_last_hour
+        count(1)::real / 2 as pages_scraped_per_hour,
+        sum(case when error is not null and error != '' then 1 else 0 end)::real / 2 as errors_per_hour
     from
         page_new
     where
-        scrape_updated > now() - interval '1 hour'
+        scrape_updated > now() - interval '2 hours'
 );
 
-update logs.green_scrape set error_rate = errors_last_hour::real / pages_scraped_last_hour where time = now();
+update logs.green_scrape set error_rate = errors_per_hour::real / pages_scraped_per_hour where time = now();
+
+insert into logs.green_scrape_by_endpoint (
+    select * from (
+        select
+            now() as time,
+            endpoint_id,
+            count(1)::real / 2 as pages_scraped_per_hour,
+            sum(case when error is not null and error != '' then 1 else 0 end)::real / 2 as errors_per_hour
+        from
+            page_new
+        where
+            scrape_updated > now() - interval '2 hours'
+        group by endpoint_id
+    ) x
+    where errors_per_hour > 0
+);
+
+update logs.green_scrape_by_endpoint set error_rate = errors_per_hour::real / pages_scraped_per_hour where time = now();
 
 commit;
