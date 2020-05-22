209e6a8e36290c9b9b52ac7b5b041d99ce710565
Richard Orr <richard@tate.rs>
2020-05-22 14:20:52 -0400

    log more green scrape stats

--- log-green-scrape-stats.sql
+++ log-green-scrape-stats.sql
@@ -31,4 +31,32 @@ insert into logs.green_scrape_by_endpoint (
 
 update logs.green_scrape_by_endpoint set error_rate = errors_per_hour::real / greatest(pages_scraped_per_hour, 1) where time = now();
 
+create temp table tmp_scrape_2hr as (select scrape_version, scrape_license, scrape_pdf_url is not null as has_pdf, scrape_metadata_url is not null as has_metadata_url from page_new where scrape_updated > now() - interval '2 hours');
+create temp table tmp_scrape_8hr as (select scrape_version, scrape_license, scrape_pdf_url is not null as has_pdf, scrape_metadata_url is not null as has_metadata_url from page_new where scrape_updated > now() - interval '8 hours');
+
+insert into logs.green_scrape_versions (
+    select now(), interval '2 hours' as interval, scrape_version, count(*) as count from tmp_scrape_2hr group by 1, 2, 3
+    union
+    select now(), interval '8 hours' as interval, scrape_version, count(*) as count from tmp_scrape_8hr group by 1, 2, 3
+);
+
+insert into logs.green_scrape_licenses (
+    select now(), interval '2 hours' as interval, scrape_license, count(*) as count from tmp_scrape_2hr group by 1, 2, 3
+    union
+    select now(), interval '8 hours' as interval, scrape_license, count(*) as count from tmp_scrape_8hr group by 1, 2, 3
+);
+
+insert into logs.green_scrape_pdf_urls (
+    select now(), interval '2 hours' as interval, has_pdf, count(*) as count from tmp_scrape_2hr group by 1, 2, 3
+    union
+    select now(), interval '8 hours' as interval, has_pdf, count(*) as count from tmp_scrape_8hr group by 1, 2, 3
+);
+
+insert into logs.green_scrape_metadata_urls (
+    select now(), interval '2 hours' as interval, has_metadata_url, count(*) as count from tmp_scrape_2hr group by 1, 2, 3
+    union
+    select now(), interval '8 hours' as interval, has_metadata_url, count(*) as count from tmp_scrape_8hr group by 1, 2, 3
+);
+
+
 commit;
