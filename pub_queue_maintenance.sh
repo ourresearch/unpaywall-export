f020b13ede38cfa9b025bbb1428052a27fe947db
Richard Orr <richard@tate.rs>
2020-07-29 16:53:44 -0400

    vacuumm page_new and pmh_record in parallel

--- pub_queue_maintenance.sh
+++ pub_queue_maintenance.sh
@@ -41,27 +41,39 @@ psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WH
     psql $DATABASE_URL -c "update pub_refresh_queue_aux set started = null where started is not null"
     psql $DATABASE_URL -c "vacuum verbose analyze pub_refresh_queue"
     psql $DATABASE_URL -c "vacuum verbose analyze pub_refresh_queue_aux"
-    heroku ps:scale refresh=$REFRESH_WORKERS --app=articlepage
-    heroku ps:scale refresh_aux=$REFRESH_AUX_WORKERS --app=articlepage
 ) & refresh_vac=$!
+
+(
+    psql $DATABASE_URL -c "vacuum verbose analyze page_new"
+) & page_vac=$!
+
+(
+    psql $DATABASE_URL -c "vacuum verbose analyze pmh_record"
+) & pmh_vac=$!
 
 
 (
     psql $DATABASE_URL -c "update page_green_scrape_queue set started = null where started is not null"
     psql $DATABASE_URL -c "update endpoint set last_harvest_started = null where last_harvest_started is not null"
     psql $DATABASE_URL -c "vacuum verbose analyze page_green_scrape_queue"
-    psql $DATABASE_URL -c "vacuum verbose analyze page_new"
-    psql $DATABASE_URL -c "vacuum verbose analyze pmh_record"
-    heroku ps:scale green_scrape=$GREEN_SCRAPE_WORKERS --app=oadoi
-    heroku ps:scale run_repo=$REPO_HARVEST_WORKERS --app=oadoi
 ) & green_scrape_vac=$!
 
 (
     psql $DATABASE_URL -c "update pdf_url_check_queue set started = null where started is not null"
     psql $DATABASE_URL -c "vacuum verbose analyze pdf_url_check_queue"
+    psql $DATABASE_URL -c "vacuum verbose analyze pdf_url"
     heroku ps:scale run_pdf_url_check=$PDF_CHECK_WORKERS --app=articlepage
 ) & pdf_check_vac=$!
 
-wait $update_vac $refresh_vac $green_scrape_vac $pdf_check_vac
+psql $DATABASE_URL -c "vacuum verbose analyze pub"
 
+wait $green_scrape_vac $page_vac $pmh_vac
+
+heroku ps:scale green_scrape=$GREEN_SCRAPE_WORKERS --app=oadoi
+heroku ps:scale run_repo=$REPO_HARVEST_WORKERS --app=oadoi
+
+wait $update_vac $refresh_vac $pdf_check_vac
+
+heroku ps:scale refresh=$REFRESH_WORKERS --app=articlepage
+heroku ps:scale refresh_aux=$REFRESH_AUX_WORKERS --app=articlepage
 heroku ps:scale update=$UPDATE_WORKERS --app=oadoi
