b02c27de5083d5b5bb10e4a437687c454be9edc3
Richard Orr <richard@tate.rs>
2020-07-29 14:14:37 -0400

    vacuum page_new and pmh_record while queues are suspended

--- pub_queue_maintenance.sh
+++ pub_queue_maintenance.sh
@@ -19,12 +19,14 @@ UPDATE_WORKERS=$(heroku ps -a oadoi update | grep '^update\.' | wc -l)
 REFRESH_WORKERS=$(heroku ps -a articlepage refresh | grep '^refresh\.' | wc -l)
 REFRESH_AUX_WORKERS=$(heroku ps -a articlepage refresh_aux | grep '^refresh_aux\.' | wc -l)
 GREEN_SCRAPE_WORKERS=$(heroku ps -a oadoi green_scrape | grep '^green_scrape\.' | wc -l)
+REPO_HARVEST_WORKERS=$(heroku ps -a oadoi run_repo | grep '^run_repo\.' | wc -l)
 PDF_CHECK_WORKERS=$(heroku ps -a articlepage run_pdf_url_check | grep '^run_pdf_url_check\.' | wc -l)
 
 heroku ps:scale update=0 --app=oadoi
 heroku ps:scale refresh=0 --app=articlepage
 heroku ps:scale refresh_aux=0 --app=articlepage
 heroku ps:scale green_scrape=0 --app=oadoi
+heroku ps:scale run_repo=0 --app=oadoi
 heroku ps:scale run_pdf_url_check=0 --app=articlepage
 
 psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE state = 'idle in transaction'";
@@ -46,8 +48,12 @@ psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WH
 
 (
     psql $DATABASE_URL -c "update page_green_scrape_queue set started = null where started is not null"
+    psql $DATABASE_URL -c "update endpoint set last_harvest_started = null where last_harvest_started is not null"
     psql $DATABASE_URL -c "vacuum verbose analyze page_green_scrape_queue"
+    psql $DATABASE_URL -c "vacuum verbose analyze page_new"
+    psql $DATABASE_URL -c "vacuum verbose analyze pmh_record"
     heroku ps:scale green_scrape=$GREEN_SCRAPE_WORKERS --app=oadoi
+    heroku ps:scale run_repo=$REPO_HARVEST_WORKERS --app=oadoi
 ) & green_scrape_vac=$!
 
 (
