d285b059e957e0301144887babc918f7ae0931a1
Richard Orr <richard@tate.rs>
2020-08-07 13:26:14 -0400

    update PMC published dates when loading csv

--- pmc-sync/load-pmc-lookup.sql
+++ pmc-sync/load-pmc-lookup.sql
@@ -2,8 +2,23 @@ create temp table tmp_pmcid_lookup (doi text, pmcid text, release_date text);
 \copy tmp_pmcid_lookup from _CSV_FILE_ csv header
 
 begin;
-delete from pmcid_lookup;
+truncate pmcid_lookup;
 insert into pmcid_lookup (doi, pmcid, release_date) (
     select lower(doi), lower(pmcid), release_date from tmp_pmcid_lookup
 );
+
+create temp table tmp_published_date as (
+    select
+        pmc.pmcid,
+        pmh.record_timestamp as published_date
+    from
+        pmcid_lookup pmc join pmh_record pmh on pmh.pmh_id = 'oai:pubmedcentral.nih.gov:' || replace(pmc.pmcid, 'pmc', '')
+    where
+        pmh.endpoint_id = 'ac9de7698155b820de7'
+        and pmc.release_date = 'live'
+        and pmh.record_timestamp is not null
+);
+
+insert into pmcid_published_date_lookup (select * from tmp_published_date where pmcid not in (select pmcid from pmcid_published_date_lookup));
+
 commit;
