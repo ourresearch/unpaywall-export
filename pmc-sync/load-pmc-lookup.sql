e0c6893720600e46cea35c05b630916c1f32890b
Richard Orr <richard@tate.rs>
2020-08-25 12:22:22 -0400

    shorten pmcid loading transaction, don't save pmh datestamps

--- pmc-sync/load-pmc-lookup.sql
+++ pmc-sync/load-pmc-lookup.sql
@@ -1,24 +1,25 @@
-create temp table tmp_pmcid_lookup (doi text, pmcid text, release_date text);
-\copy tmp_pmcid_lookup from _CSV_FILE_ csv header
+create table tmp_pmcid_lookup (like pmcid_lookup including all);
+\copy tmp_pmcid_lookup (doi, pmcid, release_date) from _CSV_FILE_ csv header
+
+update tmp_pmcid_lookup set doi = lower(doi), pmcid = lower(pmcid);
 
 begin;
-truncate pmcid_lookup;
-insert into pmcid_lookup (doi, pmcid, release_date) (
-    select lower(doi), lower(pmcid), release_date from tmp_pmcid_lookup
-);
 
-create temp table tmp_published_date as (
-    select
-        pmc.pmcid,
-        pmh.record_timestamp as published_date
-    from
-        pmcid_lookup pmc join pmh_record pmh on pmh.pmh_id = 'oai:pubmedcentral.nih.gov:' || replace(pmc.pmcid, 'pmc', '')
-    where
-        pmh.endpoint_id = 'ac9de7698155b820de7'
-        and pmc.release_date = 'live'
-        and pmh.record_timestamp is not null
-);
-
-insert into pmcid_published_date_lookup (select * from tmp_published_date where pmcid not in (select pmcid from pmcid_published_date_lookup));
+drop table pmcid_lookup;
+alter table tmp_pmcid_lookup rename to pmcid_lookup;
 
 commit;
+
+--create temp table tmp_published_date as (
+--    select
+--        pmc.pmcid,
+--        pmh.record_timestamp as published_date
+--    from
+--        pmcid_lookup pmc join pmh_record pmh on pmh.pmh_id = 'oai:pubmedcentral.nih.gov:' || replace(pmc.pmcid, 'pmc', '')
+--    where
+--        pmh.endpoint_id = 'ac9de7698155b820de7'
+--        and pmc.release_date = 'live'
+--        and pmh.record_timestamp is not null
+--);
+--
+--insert into pmcid_published_date_lookup (select * from tmp_published_date where pmcid not in (select pmcid from pmcid_published_date_lookup));
