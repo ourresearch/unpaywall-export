ebaa090b0978004b9aed688393dc455db5eee250
Richard Orr <richard@tate.rs>
2020-11-05 17:42:06 -0500

    collect green scrape stats over a longer time span

--- log-green-scrape-stats.sql
+++ log-green-scrape-stats.sql
@@ -3,12 +3,12 @@ begin;
 insert into logs.green_scrape (
     select
         now() as time ,
-        count(1)::real / 2 as pages_scraped_per_hour,
-        sum(case when error is not null and error != '' then 1 else 0 end)::real / 2 as errors_per_hour
+        count(1)::real / 8 as pages_scraped_per_hour,
+        sum(case when error is not null and error != '' then 1 else 0 end)::real / 8 as errors_per_hour
     from
         page_new
     where
-        scrape_updated > now() - interval '2 hours'
+        scrape_updated > now() - interval '8 hours'
 );
 
 update logs.green_scrape set error_rate = errors_per_hour::real / greatest(pages_scraped_per_hour, 1) where time = now();
@@ -18,12 +18,12 @@ insert into logs.green_scrape_by_endpoint (
         select
             now() as time,
             endpoint_id,
-            count(1)::real / 2 as pages_scraped_per_hour,
-            sum(case when error is not null and error != '' then 1 else 0 end)::real / 2 as errors_per_hour
+            count(1)::real / 8 as pages_scraped_per_hour,
+            sum(case when error is not null and error != '' then 1 else 0 end)::real / 8 as errors_per_hour
         from
             page_new
         where
-            scrape_updated > now() - interval '2 hours'
+            scrape_updated > now() - interval '8 hours'
         group by endpoint_id
     ) x
     where errors_per_hour > 0
