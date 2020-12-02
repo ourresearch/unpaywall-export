63a3643ac1ca164587a237e235dc3375ac5e8679
Richard Orr <richard@tate.rs>
2020-12-02 13:24:32 -0500

    log oa rates for top 20 publishers

--- log-oa-rates-by-publisher.sh
+++ log-oa-rates-by-publisher.sh
@@ -21,6 +21,20 @@ select * from (
             when regexp_contains(publisher, r'(?i)\\binforma\\b') or (regexp_contains(publisher, r'(?i)\\btaylor\\b') and regexp_contains(publisher, r'(?i)\\bfrancis\\b')) then 'Taylor & Francis'
             when regexp_contains(publisher, r'(?i)\\boxford university press\\b') then 'OUP'
             when regexp_contains(publisher, r'(?i)\\bsage publication') then 'SAGE'
+            when regexp_contains(publisher, r'\\bMDPI\\b') then 'MDPI AG'
+            when regexp_contains(publisher, r'(?i)\\binstitute of electrical and electronics engineers\\b') or regexp_contains(publisher, r'(?i)\\bieee\\b') then 'IEEE'
+            when regexp_contains(publisher, r'(?i)\\bamerican chemical society\\b') then 'American Chemical Society'
+            when regexp_contains(publisher, r'(?i)\\bIOP Publishing\\b') then 'IOP Publishing'
+            when regexp_contains(publisher, r'(?i)\\bWolters Kluwer Health\\b') then 'Wolters Kluwer Health'
+            when regexp_contains(publisher, r'(?i)\\bRoyal Society of Chemistry\\b') then 'Royal Society of Chemistry'
+            when regexp_contains(publisher, r'(?i)\\bCambridge University Press\\b') then 'Cambridge University Press'
+            when regexp_contains(publisher, r'(?i)\\bGeorg Thieme\\b') then 'Georg Thieme Verlag KG'
+            when regexp_contains(publisher, r'(?i)\\bPublic Library of Science\\b') then 'Public Library of Science'
+            when regexp_contains(publisher, r'(?i)\\bBMJ Publishing\\b') then 'BMJ Publishing'
+            when regexp_contains(publisher, r'(?i)\\bWalter de Gruyter\\b') then 'Walter de Gruyter'
+            when regexp_contains(publisher, r'(?i)\\bSciELO\\b') then 'SciELO'
+            when regexp_contains(publisher, r'(?i)\\bHindawi Limited\\b') then 'Hindawi Limited'
+            when regexp_contains(publisher, r'(?i)\\bEDP Sciences\\b') then 'EDP Sciences'
             else null
         end as publisher,
         oa_status,
