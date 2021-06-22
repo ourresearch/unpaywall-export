7084f24ee3f7d29420e71aa56389941241240295
Richard Orr <richard@tate.rs>
2021-06-22 14:13:48 -0400

    log trailing 8 hour oa_status change stats for refreshed dois

--- logging_update.sh
+++ logging_update.sh
@@ -22,6 +22,17 @@ psql $DATABASE_URL -c "refresh materialized view pdf_check_rate_mv";
 psql $DATABASE_URL -c "insert into pub_refresh_overdue_fraction (select now(),  1.0 * (select sum(count) from pub_refresh_priority_histo_mv where priority > 1) / (select sum(count) from pub_refresh_priority_histo_mv))";
 
 psql $DATABASE_URL -c "\
+    insert into logs.refresh_oa_status_results_trailing_8_hr ( \
+        time, oa_status_before, oa_status_after, num_dois \
+    ) ( \
+        select now(), oa_status_before, oa_status_after, count(distinct id) as num_dois \
+        from pub_refresh_result \
+        where refresh_time >= now() - interval '8 hours' \
+        group by 1, 2, 3 \
+    );"
+
+
+psql $DATABASE_URL -c "\
     insert into pub_volatility ( \
         select \
             now() as time, \
