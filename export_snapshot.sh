077d7c42bfe2d9344acdc15601fd3ec12b19a2f7
Richard Orr <richard@tate.rs>
2021-07-02 10:52:50 -0400

    only include DOIs with non-null oa_status in snapshot

--- export_snapshot.sh
+++ export_snapshot.sh
@@ -43,7 +43,7 @@ logger "Process  : $PROCESS"
 logger "Filename : $FILENAME"
 
 logger "Exporting database column to file"
-/usr/bin/psql "${DATABASE_URL}?ssl=true" -c "\copy (select response_jsonb from pub where response_jsonb is not null) to '${FILENAME}';"
+/usr/bin/psql "${DATABASE_URL}?ssl=true" -c "\copy (select response_jsonb from pub where response_jsonb is not null and response_jsonb->>'oa_status' is not null) to '${FILENAME}';"
 PSQL_EXIT_CODE=$?
 
 if [[ $PSQL_EXIT_CODE -ne 0 ]] ; then
