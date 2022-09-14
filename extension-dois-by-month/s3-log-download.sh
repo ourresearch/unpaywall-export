7cd123d872ad42a046ae72d3ab3fab59d065e3ba
Richard Orr <richard@ourresearch.org>
2022-09-14 11:38:24 -0400

    account for change in s3 log file paths

--- extension-dois-by-month/s3-log-download.sh
+++ extension-dois-by-month/s3-log-download.sh
@@ -10,7 +10,13 @@ print_date=$(echo $date_str | cut -c1-10)
 
 >&2 echo "retreiving ${date_str}"
 
-aws s3 cp "s3://ourresearch-papertrail/logs/dt=${print_date}/${date_str}.tsv.gz" - \
+if [[ "$print_date" < "2019-08-17" ]]; then
+    s3_object="s3://ourresearch-papertrail/logs/${date_str}.tsv.gz"
+else
+    s3_object="s3://ourresearch-papertrail/logs/dt=${print_date}/${date_str}.tsv.gz"
+fi
+
+aws s3 cp "$s3_object" - \
 | gunzip -c - \
 | grep 'logthis:' \
 | grep '"email": "unpaywall@impactstory.org"' \
